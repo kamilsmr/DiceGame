ec63c2bc18a45c631b3545c105297965f1eb2554
kamilsmr <kamils6035@gmail.com>
2022-10-26 22:19:49 +0300

    style

---
+++ app.js
@@ -0,0 +1,8 @@
+const button = document.querySelector("button");
+const player1 = document.querySelector("player1");
+const player2 = document.querySelector("player2");
+const output = document.querySelector("output");
+button.addEventListener("click",function(){
+    console.log("works");
+})
+
